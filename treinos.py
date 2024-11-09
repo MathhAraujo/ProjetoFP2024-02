997f44ffb1db33198be11e0145ce6eef8b1b1914
LeoGutzeitt <leonardogbmatos@hotmail.com>
2024-11-09 18:20:16 -0300

    matriz
    guardando dados na matriz

---
+++ treinos.py
@@ -0,0 +1,11 @@
+Q = int(input('Quantos treinos: '))
+treinos = []
+tempos = []
+
+for i in range(Q):
+    treino = input("Digite a data do treino: \n")
+    tempo = input("Digite o seu tempo: \n")
+    treinos.append(treino)
+    tempos.append(tempo)
+
+
