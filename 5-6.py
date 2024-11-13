dda4c7ae7f7344ead5b3e52bc46c9ab20c2f355e
Matheus <mhma@cesar.school>
2024-11-13 15:28:41 -0300

    Add função de adicionar objetivos
    Objetivo e seu valor separados por "/"

--- 5-6.py
+++ 5-6.py
@@ -5,7 +5,14 @@ def arquivoHandler(openingFormat):
     try:
         arquivo = open("5-6.txt", openingFormat)
     except FileNotFoundError:
-        open("5-6.txt", "x")
+        open("5-6.txt", "x").close()
         arquivo = arquivoHandler(openingFormat)
 
     return arquivo
+
+def addObjetivo(objetivo, valor):
+    arquivo = arquivoHandler("a")
+
+    arquivo.write(f"{objetivo}/{valor}\n")
+
+    arquivo.close();
