965d47bb31fa3450c290eba58ad4c6596c1cf145
Matheus <mhma@cesar.school>
2024-11-13 16:08:23 -0300

    Add atualizar objetivo
    Adicionada função de atualizar objetivo

--- 5-6.py
+++ 5-6.py
@@ -10,9 +10,37 @@ def arquivoHandler(openingFormat):
 
     return arquivo
 
-def addObjetivo(objetivo, valor):
+def addObjetivo(objetivo, valor, medida):
     arquivo = arquivoHandler("a")
 
-    arquivo.write(f"{objetivo}/{valor}\n")
+    arquivo.write(f"{objetivo}/{float(valor)}/{medida}\n")
 
     arquivo.close();
+
+def atualizarObjetivo(objetivo, valorModificado):
+    arquivo = arquivoHandler("r")
+
+    objetivoLista = arquivo.readlines()
+
+    if len(objetivoLista)== 0:
+        print("Nenhum Objetivo registrado")
+    else:
+        newObjetivoList = []
+
+        for i in objetivoLista:
+
+            i = i.split("/")
+            if i[0] == objetivo:
+                i[1]= float(i[1])
+                i[1] -= valorModificado
+
+            newObjetivoList.append(f"{i[0]}/{i[1]}/{i[2]}")
+
+    arquivo.close()
+    
+    arquivo = arquivoHandler("w")   
+    
+    for i in newObjetivoList:
+        arquivo.write(f"{i}")
+    
+    arquivo.close()
